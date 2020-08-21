de212a96b865b93ebe0e25ff9102b3ecc3a7d087
smdeox <smdeox@live.com>
2020-08-21 17:59:33 -0400

    New problem

---
+++ VideoPractice.js
@@ -0,0 +1,18 @@
+// you got a list of items, where every item has a value and a weight. You got a bag that holds a maximum weight of X. Write a program thatmaximezses the value of the items you put in the bag while ensuring that you don't eceed the maximum weight.
+
+items = [
+  {
+    id: 'a', val: 10, w:3
+  },
+  {
+    id: 'b', val: 6, w:8
+  },
+  {
+    id: 'c', val: 3, w:3
+  }
+]
+
+maxWeight = 8 // Solution
+
+bag = ['a', 'c']
+// Value = 13, Weight = 6
